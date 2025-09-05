cd675cc0278b805240220781f6e0a16becaa302f
itdep <itdep@ASUS-SOLAPTOP>
2025-09-05 16:14:25 +0800

    final commit

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "https://chat-app-backend-1-istv.onrender.com" : "/api",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
